927eecc2e2c5bc027c27aa3191002f02e95879ba
raghu <raghu@elantecs.com>
2017-12-24 22:24:01 +0000

    kitchen commands file

---
+++ kitchen_commands.sh
@@ -0,0 +1,12 @@
+kitchen <option>   #You can choose below options
+       
+
+        init           #created a boiler plate .kitchen.yml
+        list           # list all instances
+        create         #create one or more instances 
+        converge       #converge one or more instance 
+        verify         #verify one or more instances
+        destory        #destory one or more instances 
+        test           #executes kitchen { destory, create, converge, verify, desotry} all in one
+        login          #log in to one instance
+        help           #provides a list of available kitchen commands
